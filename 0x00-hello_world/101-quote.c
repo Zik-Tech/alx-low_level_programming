d619055c6e242081d24bc462021403ad733805f5
unique-unice <adioeunice0@gmail.com>
2022-09-08 14:28:19 -0700

    testing quote in C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Descriptino: it is about printing with printf without special functios.
+ *
+ * Return: Always 1 (success).
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Kopar,2015-10-19");
+	return (1);
