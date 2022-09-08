9c60c421fc81e32ab08815b1fb81358f5462447c
unique-unice <adioeunice0@gmail.com>
2022-09-08 11:47:46 -0700

    i hope this works

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) /**
+		* Description: i called the main function.
+		*/
+{
+	/* this is a program that prints a line of word */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
