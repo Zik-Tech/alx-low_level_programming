4665082f4225ce79f8edf2d04ccd4079e7e5763c
unique-unice <adioeunice0@gmail.com>
2022-09-08 12:22:33 -0700

    using printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main
+ *
+ * Description: this code prints a line of words using printf.
+ *
+ * the code returns 0 signifing sucess
+ */
+int main(void)
+	/* this is the main part of the code*/
+	printf("with proper grammar\, but the outcome is a piece of art\,");
+	return (0);
