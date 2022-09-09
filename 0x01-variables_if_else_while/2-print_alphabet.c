92f5af17db994e5f041e904b6f1699fbb19c84f4
unique-unice <adioeunice0@gmail.com>
2022-09-09 12:52:25 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,8 +13,8 @@ int main(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-	putchar(alpha);
-	alpha = tolower(alpha);
+		putchar(alpha);
+		alpha = tolower(alpha);
 	}
 	putchar('\n');
 	return (0);
