f2d91940e2e8367bc87720584a8a5fe5f24c8e8c
unique-unice <adioeunice0@gmail.com>
2022-09-09 15:11:07 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints 0-9 using for loop.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+return (0);
+}
